617349b090e9312638909e5c2b8f8b17deaeae61
Irfan Zaky Harlen <irfanzakyharlen@gmail.com>
2020-08-24 15:01:15 +0800

    1.6: unicafe step1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,62 +1,62 @@
-import  React  from  'react'
-import  ReactDOM  from  'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const  Header  =  (props)  =>{
-    return(
-        <h1>{props.name}</h1>
-    )
-}
+const Header = (props) => {
+  return <h1>{props.name}</h1>;
+};
 
 const Part = (props) => {
   return (
     <p>
-      {props.content.name}  {props.content.exercise}
+      {props.content.name} {props.content.exercise}
     </p>
-  )
-}
+  );
+};
 
-const  Content  =  (props)  => {
+const Content = (props) => {
   return (
     <div>
-      <Part content = {props.content[0]}/>
-      <Part content = {props.content[1]}/>
-      <Part content = {props.content[2]}/>
+      <Part content={props.content[0]} />
+      <Part content={props.content[1]} />
+      <Part content={props.content[2]} />
     </div>
-  )
-}
+  );
+};
 
+const Total = (props) => {
+  return <p>Number of exercises {props.totalExcercise}</p>;
+};
 
-const  Total  =  (props)  =>{
-    return(
-        <p>Number  of  exercises  {props.totalExcercise}</p>
-    )
-}
-
-const  App  =  ()  =>  {
-    const course = {
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-    }
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header name = {course.name}  />
-            <Content  content  =  {course.parts}/>
-            <Total  totalExcercise  =  {course.parts.reduce((acc, part)  =>  acc  +  part.exercise,  0)}/>
-        </div>
-    )
-}
+      <Header name={course.name} />
+      <Content content={course.parts} />
+      <Total
+        totalExcercise={course.parts.reduce(
+          (acc, part) => acc + part.exercise,
+          0
+        )}
+      />
+    </div>
+  );
+};
 
-ReactDOM.render(<App  />,  document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+
+      <h1>statistic</h1>
+
+      <p>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+        <br />
+        all {good + neutral + bad}
+        <br />
+        average
+        <br />
+        positive
+      </p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
