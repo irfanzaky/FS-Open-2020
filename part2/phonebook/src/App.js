53c7f528f551cfb22bd834c6621af0ddbc6448e3
Irfan Zaky Harlen <irfanzakyharlen@gmail.com>
2020-08-25 18:28:43 +0800

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Ada Loves", number: "19-24-5431" }]);
   const [newName, setNewName] = useState("Irfan");
   const [newNumber, setNewNumber] = useState("831-1823-1238");
+  const [newSearch, setNewSearch] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
 
   function handleAddName(event) {
     event.preventDefault();
@@ -28,10 +34,24 @@ const App = () => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
   }
+  function handleSearchChange(event) {
+    console.log(event.target.value, filteredPerson);
+    setNewSearch(event.target.value);
+  }
+
+  const filteredPerson = persons.filter((person) => person.name.includes(newSearch));
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        filter shown with:
+        <input value={newSearch} onChange={handleSearchChange} />
+        <br />
+      </form>
+      <Phonebook persons={filteredPerson} />
+
+      <h2>Add new person</h2>
       <form onSubmit={handleAddName}>
         name: <input value={newName} onChange={handleNameChange} />
         <br />
@@ -40,8 +60,8 @@ const App = () => {
         <button type="submit">add</button>
         <br />
       </form>
-      <h2>Numbers</h2>
 
+      <h2>Numbers</h2>
       <Phonebook persons={persons} />
       <div>debug: {newName}</div>
     </div>
