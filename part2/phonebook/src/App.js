58ca4f947dcfa69e03dcf8527b55b212d65df43f
Irfan Zaky Harlen <irfanzakyharlen@gmail.com>
2020-08-25 15:17:36 +0800

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,39 +1,44 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("No new name");
+  const [persons, setPersons] = useState([{ name: "Ada Loves", number: "19-24-5431" }]);
+  const [newName, setNewName] = useState("Irfan");
+  const [newNumber, setNewNumber] = useState("831-1823-1238");
 
   function handleAddName(event) {
     event.preventDefault();
-    const newNametoAdd = { name: newName };
+    const newPersontoAdd = { name: newName, number: newNumber };
     const checkPerson = persons.filter((person) => person.name === newName).length;
-    //console.log(persons, newNametoAdd, checkPerson);
+    //console.log(persons, newPersontoAdd, checkPerson);
 
     if (checkPerson) {
       alert(`${newName} is already added to phonebook`);
       console.log(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(newNametoAdd));
+      setPersons(persons.concat(newPersontoAdd));
       console.log("new name added", event.target);
     }
   }
 
-  function handleInputChange(event) {
+  function handleNameChange(event) {
     console.log(event.target.value);
     setNewName(event.target.value);
+  }
+  function handleNumberChange(event) {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleAddName}>
-        <div>
-          name: <input value={newName} onChange={handleInputChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        name: <input value={newName} onChange={handleNameChange} />
+        <br />
+        number: <input value={newNumber} onChange={handleNumberChange} />
+        <br />
+        <button type="submit">add</button>
+        <br />
       </form>
       <h2>Numbers</h2>
 
@@ -44,7 +49,11 @@ const App = () => {
 };
 
 const Phonebook = ({ persons }) => {
-  return persons.map((person) => <div key={person.name}>{person.name}</div>);
+  return persons.map((person) => (
+    <div key={person.name}>
+      {person.name} {person.number}
+    </div>
+  ));
 };
 
 export default App;
